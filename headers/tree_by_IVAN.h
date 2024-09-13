4bd849dbf7910c2f1fad0462b9295c521ab575a3
Mirovengil <Mirivengil@gmail.com>
2024-09-13 21:18:12 +0300

    Прописал Node (функции создания/удаления) и очень ею горжусь.

--- headers/tree_by_IVAN.h
+++ headers/tree_by_IVAN.h
@@ -1,6 +1,27 @@
 #ifndef TREE_BY_IVAN_H
 #define TREE_BY_IVAN_H
 
+enum NodeTypes
+{
+    FILE, 
+    FOLDER,
+    NUMBER_OF_NODE_TYPES
+};
 
+struct Node
+{
+    enum NodeTypes type;    // папка или файл?
+
+    // в переменной data может быть два типа данных:
+    // 1. если type == FILE, то в переменную data побайтово копируется содержание файла
+    // 2. если type == FOLDER, то в переменную data сначала помещается переменная типа int --
+    // число объектов в данной папке, а потом указатели на Node -- на объекты в папке 
+    char *data; 
+    char *name;         // имя файла/папки
+};
+
+struct Node* createNewFolderNode(const char *directory_name);
+struct Node* createNewFileNode(const char *directory_name);
+void deleteTree(struct Node* tree);
 
 #endif //TREE_BY_IVAN_H
--- src/tree_by_IVAN.c
+++ src/tree_by_IVAN.c
@@ -1,1 +1,67 @@
 #include "tree_by_IVAN.h"
+#include <stdlib.h>
+#include <string.h>
+
+struct Node* createNewFolderNode(const char *directory_name)
+{
+    struct Node *newTree = malloc(sizeof(struct Node));
+    
+    newTree->type = FOLDER;
+
+    newTree->data = malloc(sizeof(int));
+    *((int*)newTree->data) = 0;         // предположительно -- папка пустая
+    
+    // копируем название по-сишному
+    newTree->name = malloc(sizeof(char)*strlen(directory_name));
+    strcpy(newTree->name, directory_name);
+
+    return newTree;
+};
+
+struct Node* createNewFileNode(const char *filename)
+{
+    struct Node *newTree = malloc(sizeof(struct Node));
+
+    newTree->type = FILE;
+    newTree->data = NULL;    // пока не заполнено! 
+
+    // копируем название по-сишному
+    newTree->name = malloc(sizeof(char)*strlen(filename));
+    strcpy(newTree->name, filename);
+
+    return newTree; 
+};
+
+void deleteTree(struct Node* tree)
+{
+    // если папка -- удаляем всё содержимое как отдельные Node
+    if (tree->type == FOLDER)
+    {
+        int numberOfObjects = *((int*)tree->data);
+
+        tree->data = (int*)tree->data + 1;  // сдвигаем указатель на первый объект в списке
+        for (int i = 0; i < numberOfObjects; ++i)
+        {
+            // удаляем все объекты Node в папке по очереди
+            deleteTree((struct Node*)tree->data[i]);
+        };
+
+        tree->data = (int*)tree->data + 1;  // сдвигаем указатель обратно
+        
+        // и просто очищаем выделенную память
+        free(tree->data);
+        free(tree->name);
+
+        // здесь можно было написать вместо двух free один конгениальный, но неочевидный ход:
+        // tree->type = FILE;
+        // тогда бы следующий if сам всё удалил. 
+        // Но это уже из разряда олимпиадного программирования))
+    };
+
+    // если файл -- просто чистим содержимое
+    if (tree->type == FILE)
+    {
+        free(tree->data);
+        free(tree->name);
+    };
+};
