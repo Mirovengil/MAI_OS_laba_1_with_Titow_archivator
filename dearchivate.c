346143d7d9e19747cc89f3a7847bb2491ed704dc
Mirovengil <Mirivengil@gmail.com>
2024-09-23 16:06:13 +0300

    РАБОЧАЯ ВЕРСИЯ!!! Уже можно сдавать. Осталось прикрутити сжатие, чуть подрефакторить, сделать документ и можно сдавать!

--- dearchivate.c
+++ dearchivate.c
@@ -21,15 +21,23 @@ int main(void)
 	getBytesArrayFromFile(fileToDearchivate, &loadedTree, &loadedTreeSizeInBytes);
 	printf("Файл %s был успешно загружен из памяти!\n");
 
+	char directoryToDearchivate[MAX_LEN_OF_STRING_NAME];
+	printf("\nВведите имя директории, в которую следует распаковать файл.\n");
+	printf("Ввод: ");
+	fgets(directoryToDearchivate, MAX_LEN_OF_STRING_NAME, stdin);
+	directoryToDearchivate[strlen(directoryToDearchivate) - 1] = '\0';
 
 	struct Node *decodedTree;
 	int position = 0;
 	decodeTreeFromArrayOfBytes(&decodedTree, loadedTree, loadedTreeSizeInBytes, &position);
 
-	printf("Дерево после декодирования из файла: \n");
+	printf("Содержимое файла: \n");
 	printTree(decodedTree);
 
-	formDirectoryWithTree(decodedTree, "./");
+	formDirectoryWithTree(decodedTree, directoryToDearchivate);
+
+	printf("Файл был успешно распакован!\n");
+	printf("\nБлагодарю за использование моей программы...\n");
 
 	deleteTree(decodedTree);
 	free(loadedTree);
